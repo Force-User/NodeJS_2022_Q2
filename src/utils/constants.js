b05b60641ff86268f5202d080ad665f4da8a71d5
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-08 17:55:30 +0300

    refactor: add FLAGS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,30 @@
-import os from 'os';
+import os from "os";
 
-export const FLAG = '--username';
-export const greatingMessage = 'Welcome to the File Manager';
-export const goodluckMessage = 'Thank you for using File Manager'
+export const FLAG = "--username";
+export const greatingMessage = "Welcome to the File Manager";
+export const goodluckMessage = "Thank you for using File Manager";
 export const HOME_DIR = os.homedir();
 
 //ACTIONS
 export const ACTIONS = {
-    START:'START',
-    EXIT:'EXIT',
-}
+  START: "START",
+  EXIT: "EXIT",
+  WITHOUT_FLAG: "WITHOUT_FLAG"
+};
 
+export const FLAGS = {
+  UP: "up",
+  CD: "cd",
+  LS: "ls",
+  CAT: "cat",
+  ADD: "add",
+  RN: "rn",
+  CP: "cp",
+  MV: "mv",
+  RM: "rm",
+  OS: "os",
+  HASH: "hash",
+  COMPRESS: "compress",
+  DECOMPRESS: "decompress",
+  EXIT: ".exit",
+};
