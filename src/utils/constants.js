178c77d43f411d1a4a24d5fdb5993cbd3791a869
Anatoly <Anatol_Yakushenka@epam.com>
2022-06-08 14:51:12 +0300

    feat: add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+export const FLAG = '--username';
+export const greatingMessage = 'Welcome to the File Manager';
+export const goodluckMessage = 'Thank you for using File Manager'
+export const HOME_DIR = os.homedir();
+
+//ACTIONS
+export const ACTIONS = {
+    START:'START',
+    EXIT:'EXIT',
+}
+
