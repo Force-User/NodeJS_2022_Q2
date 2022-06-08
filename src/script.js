3cb683bb57cd2f6fecce7bf01e66dfb78a552f5a
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-08 17:56:39 +0300

    refactor: refactor script file

--- src/script.js
+++ src/script.js
@@ -1,59 +1,77 @@
-import process, { stdin , stdout, exit, argv } from 'process';
-import fs from 'fs';
-import {checkUserName, printMessage} from "./utils/services.js";
-import {ACTIONS, HOME_DIR} from "./utils/constants.js";
-import path from 'path';
-import {upToDirectory} from "./utils/services/upToDirectory.service.js";
-import {showList} from "./utils/services/list.service.js";
-import {addFile} from "./utils/services/addFile.service.js";
-import {readFileService} from "./utils/services/readFile.service.js";
-import {changeDirService} from "./utils/services/changeDir.service.js";
-import {renameFileService} from "./utils/services/renameFile.service.js";
-import {copyFileService} from "./utils/services/copyFile.service.js";
+import process, { stdin, exit, argv } from "process";
+import { checkParams, checkUserName, printMessage } from "./utils/services.js";
+import { ACTIONS, FLAGS } from "./utils/constants.js";
+import { upToDirectory } from "./utils/services/up_to_directory.service.js";
+import { showList } from "./utils/services/list.service.js";
+import { addFile } from "./utils/services/addFile.service.js";
+import { readFileService } from "./utils/services/readFile.service.js";
+import { changeDirService } from "./utils/services/changeDir.service.js";
+import { renameFileService } from "./utils/services/renameFile.service.js";
+import { copyFileService } from "./utils/services/copyFile.service.js";
+import { moveFileService } from "./utils/services/moveFile.service.js";
 
 const args = argv.slice(2);
-const [currentFlag, username] = args[0].split('=');
+const [currentFlag, username] = checkParams(args);
 checkUserName(currentFlag, username);
 
-process.chdir(HOME_DIR);
-console.log(process.cwd());
+stdin.on("data", (data) => {
+  const [flag, option1, option2] = data.toString().trim().split(" ");
+  switch (flag) {
+    case FLAGS.UP:
+      upToDirectory();
+      break;
 
+    case FLAGS.CD:
+      changeDirService(option1);
+      break;
 
-stdin.on('data', (data) => {
-    const [flag, option1, option2] = data.toString().trim().split(' ');
-    switch(flag) {
-        case '.exit':
-            printMessage(username, ACTIONS.EXIT)
-            exit()
-            break;
-        case 'up' :
-            upToDirectory();
-            break;
-        case 'ls':
-            showList();
-            break;
-        case 'add':
-            addFile(option1);
-            break;
-        case 'cat' :
-            readFileService(option1);
-            break
-        case 'cd' :
-        changeDirService(option1);
-            break;
-        case 'rn':
-            renameFileService(option1, option2);
-            break;
-        case 'cp':
-            copyFileService(option1, option2);
-            break;
-    }
-})
+    case FLAGS.LS:
+      showList();
+      break;
 
+    case FLAGS.CAT:
+      readFileService(option1);
+      break;
 
-process.on('SIGINT', function(a) {
-    printMessage( username, ACTIONS.EXIT)
-    process.exit(1);
+    case FLAGS.ADD:
+      addFile(option1);
+      break;
+
+    case FLAGS.RN:
+      renameFileService(option1, option2);
+      break;
+
+    case FLAGS.CP:
+      copyFileService(option1, option2);
+      break;
+
+    case FLAGS.MV:
+      moveFileService(option1, option2);
+      break;
+
+    case FLAGS.EXIT:
+      printMessage(ACTIONS.EXIT, username);
+      exit();
+      break;
+
+    case FLAGS.RM:
+      break;
+
+    case FLAGS.OS:
+      break;
+
+    case FLAGS.HASH:
+      break;
+
+    case FLAGS.COMPRESS:
+      break;
+
+    case FLAGS.DECOMPRESS:
+      break;
+  }
 });
 
-
+process.on("SIGINT", function (a) {
+  printMessage(ACTIONS.EXIT, username);
+  process.exit(1);
+});
