3008a804e91e1a99cad8c02153823028b0a9bb73
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-08 17:56:11 +0300

    refactor: refactor services: refactoring methods

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,19 +1,40 @@
-import { stdout } from 'process';
-import {ACTIONS, FLAG, goodluckMessage, greatingMessage} from "./constants.js";
+import { stdout } from "process";
+import {
+  ACTIONS,
+  FLAG,
+  goodluckMessage,
+  greatingMessage,
+} from "./constants.js";
+
+export const checkParams = (args) => {
+  if (!args[0]) {
+    printMessage(ACTIONS.WITHOUT_FLAG);
+    process.exit();
+  }
+
+  return args[0].split("=");
+};
 
 export const checkUserName = (currentFlag, username) => {
-    if(currentFlag === FLAG) {
-        stdout.write(`${greatingMessage}, ${username}!\n`);
-    }
-}
+  currentFlag === FLAG
+    ? printMessage(ACTIONS.START, username || 'Anonimys')
+    : printMessage(ACTIONS.WITHOUT_FLAG);
+};
 
-export const printMessage = (username, action) => {
-    switch (action) {
-        case ACTIONS.START:
-            stdout.write(`${greatingMessage}, ${username}!\n`)
-            break;
-        case ACTIONS.EXIT:
-            stdout.write(`${goodluckMessage}, ${username}!\n`)
-            break;
-    }
-}
+export const printMessage = (action, username) => {
+  console.clear();
+  switch (action) {
+    case ACTIONS.START:
+      stdout.write(`${greatingMessage}, ${username}!\n`);
+      break;
+    case ACTIONS.EXIT:
+      stdout.write(`${goodluckMessage}, ${username}!\n`);
+      break;
+    case ACTIONS.WITHOUT_FLAG:
+      stdout.write(
+        `'Нужно запустить программу используя команду npm run start -- --username=your_username'!
+          Программа была завершена!`
+      );
+      break;
+  }
+};
