f2bdabef3b084ea4d5d58366fcf69725f2957fd1
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-12 20:50:46 +0300

    refactor: copy file method

--- src/modules/copy_file.module.js
+++ src/modules/copy_file.module.js
@@ -8,6 +8,11 @@ export const copyFileService = async(pathToFile, pathToNewDirectory,) => {
     console.clear();
     try{
         await access(pathToFile, fs.F_OK);
+        const fileName = pathToFile.split("\\").slice(-1).join("").trim();
+        const copyFileName = pathToNewDirectory.split("\\").slice(-1).join("").trim();
+        if(copyFileName !== fileName) {
+            pathToNewDirectory = `${pathToNewDirectory}\\${fileName}`
+        }
         await copyFile(pathToFile, pathToNewDirectory, fs.constants.COPYFILE_EXCL);
         console.log('File copied!');
     }
